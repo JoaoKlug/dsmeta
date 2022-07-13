7f3cb8cd41dcd8a814ff4ed9f5934c0b37f780f0
Joao Gabriel Klug <94715363+JoaoKlug@users.noreply.github.com>
2022-07-12 21:16:37 -0300

    Outros Componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificacionButton"
-
+import SalesCard from "./components/SalesCard"
 function App() {
   return (
     <>
-      <h1>Ola Mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
